0a5a7c6a8dc27ef8ed683d0a8abde6f7d763900b
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-03-17 10:16:57 +0530

    getting memory usage

--- src/stats.py
+++ src/stats.py
@@ -4,3 +4,13 @@ def get_cpu_usage():
     """Returns CPU usage percentage."""
     return psutil.cpu_percent(interval=1)
 
+def get_memory_usage():
+    """Returns memory usage stats (total, used, free, percent)."""
+    mem = psutil.virtual_memory()
+    return {
+        'total': mem.total,
+        'used': mem.used,
+        'free': mem.available,
+        'percent': mem.percent
+    }
+
